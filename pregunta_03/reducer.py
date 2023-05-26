dc71331ebcb13ac8d42bd85798cbda28c96d6337
Pablo Andres <codechemfin@gmail.com>
2023-05-25 22:10:05 -0500

    Update reducer.py

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,13 @@
-#
-# >>> Escriba el codigo del reducer a partir de este punto <<<
-#
+import sys
+if __name__ == "__main__":
+    data = []
+
+    for line in sys.stdin:
+        row = line.strip()
+        row = row.split('\t')
+        data.append(row)
+    
+    arrangement = sorted(data, key = lambda x: (x[1]))
+
+    for item in arrangement:
+        sys.stdout.write('{},{}\n'.format(item[0], item[1]))
