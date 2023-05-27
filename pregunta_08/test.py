e12cb654829663dd29464d32f1c1c9184afc6bbb
Pablo Andres <codechemfin@gmail.com>
2023-05-27 15:36:08 -0500

    Update test.py

--- pregunta_08/test.py
+++ pregunta_08/test.py
@@ -5,7 +5,8 @@ result = os.popen("cat data.csv | python3 mapper.py | sort | python3 reducer.py"
 
 lines = [line.strip().replace("\n", "") for line in result.split("\n")]
 
-expected = """A	165.0	13.75
+expected = """
+A	165.0	13.75
 B	136.0	13.6
 C	48.0	12.0
 D	15.0	15.0
