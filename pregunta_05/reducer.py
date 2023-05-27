0122dcae03ff00b902d64803f4153a5200b4ef4b
Pablo Andres <codechemfin@gmail.com>
2023-05-27 13:51:39 -0500

    Update reducer.py

--- pregunta_05/reducer.py
+++ pregunta_05/reducer.py
@@ -1,3 +1,22 @@
-#
-# >>> Escriba el codigo del reducer a partir de este punto <<<
-#
+import sys
+
+if __name__ == '__main__':
+
+    mes_count = {}
+
+    for line in sys.stdin:
+        row = line.strip()
+
+        mes, count = row.split('\t')
+        count = int(count)
+        mes = str(mes)
+
+        if mes in mes_count:
+            mes_count[mes] += count
+        else:
+            mes_count[mes] = count
+    
+    for mes in sorted(mes_count):
+        count = mes_count[mes]
+        sys.stdout.write("{}\t{}\n".format(mes, count))
+
