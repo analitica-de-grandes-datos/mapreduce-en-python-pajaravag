47c605aae573e36eadf31f3efa502873e8e20958
Pablo Andres <codechemfin@gmail.com>
2023-05-27 15:26:42 -0500

    Update reducer.py

--- pregunta_08/reducer.py
+++ pregunta_08/reducer.py
@@ -1,3 +1,26 @@
-#
-# >>> Escriba el codigo del reducer a partir de este punto <<<
-#
+import sys
+
+if __name__ == '__main__':
+
+    word_count = {}
+    word_times = {}
+
+    for line in sys.stdin:
+        row = line.strip()
+
+        word, times, count = row.split('\t')
+        count = int(count)
+        word = str(word)
+        times = int(times)
+
+        if word in word_count:
+            word_count[word] += count
+            word_times[word] += times
+        else:
+            word_count[word] = count
+            word_times[word] = times
+    
+    for word in sorted(word_count):
+        counter = word_count[word]
+        average_word = word_count[word] / word_times[word]
+        sys.stdout.write("{}\t{}\t{}\n".format(word, counter, average_word))
