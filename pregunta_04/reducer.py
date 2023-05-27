4ac9c697d10788b1a5a39e579f58f52440d89dde
Pablo Andres <codechemfin@gmail.com>
2023-05-27 12:13:20 -0500

    Update reducer.py

--- pregunta_04/reducer.py
+++ pregunta_04/reducer.py
@@ -1,3 +1,15 @@
-#
-# >>> Escriba el codigo del reducer a partir de este punto <<<
-#
+import sys
+from datetime import datetime
+
+if __name__ == "__main__":
+    data = []
+
+    for line in sys.stdin:
+        row = line.strip()
+        row = row.split('\t')
+        data.append(row)
+
+    arrangement = sorted(data, key = lambda x: datetime.strptime(x[1], '%Y-%m-%d'))
+
+    for item in arrangement:
+        sys.stdout.write('{}\t{}\t{}\n'.format(item[0], item[1], item[2]))
