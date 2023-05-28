8cfd0b349af48b8ab2a11c4c1a965fadf67943e5
Pablo Andres <codechemfin@gmail.com>
2023-05-28 11:44:20 -0500

    Update mapper.py

--- pregunta_10/mapper.py
+++ pregunta_10/mapper.py
@@ -1,3 +1,14 @@
-#
-# >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+import sys
+
+if __name__=="__main__":
+
+    for line in sys.stdin:
+
+        row = line.strip().split('\t')
+
+        for item in row[1]:
+
+            word = item.strip().split(',')
+
+            if len(word) == 1:
+                sys.stdout.write("{}\t{}\n".format(word[0], row[0]))
