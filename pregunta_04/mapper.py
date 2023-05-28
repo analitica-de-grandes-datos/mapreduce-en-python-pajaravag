6da286acc51db43886b64d36364f8b8d9273149a
Pablo Andres <codechemfin@gmail.com>
2023-05-28 10:33:08 -0500

    Update mapper.py

--- pregunta_04/mapper.py
+++ pregunta_04/mapper.py
@@ -1,7 +1,17 @@
-import sys
-if __name__ == "__main__":
+# import sys
+# if __name__ == "__main__":
 
-    for line in sys.stdin:
-        row = line.strip()
-        row = row.split('   ')
-        sys.stdout.write('{}\t{}\t{}\n'.format(row[0], row[1], row[2]))
+#     for line in sys.stdin:
+#         row = line.strip()
+#         row = row.split('   ')
+#         sys.stdout.write('{}\t{}\t{}\n'.format(row[0], row[1], row[2]))
+
+import sys
+
+for row in sys.stdin:
+    elementos = row.strip().split(",")
+    campos = elementos[0]
+    column = campos.split("   ")
+    linea = column[0]
+    #linea = ",".join(column)
+    sys.stdout.write((linea+"\t1\n"))
